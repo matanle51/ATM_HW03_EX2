56b13565d3c71788b5250a2dd03d819e40ae9d1a
David Lazar <davidlazar81@gmail.com>
2017-12-15 17:01:19 +0200

    changed buffer size from 100 to 256

--- ATM_HW03_EX2/injector-2/injector.cpp
+++ ATM_HW03_EX2/injector-2/injector.cpp
@@ -10,11 +10,11 @@ const char* injectedDllName = "C:\\Users\\IEUser\\Desktop\\injected.dll";
 
 int main(int argc, char **argv)
 {
-	char debugBuf[100];
+	char debugBuf[256];
 
 	// Check for correct number of arguments - 1 argument is excepted
 	if (argc != 2) {
-		sprintf_s(debugBuf, 100, "[Error] Wrong number of arguments");
+		sprintf_s(debugBuf, 256, "[Error] Wrong number of arguments");
 		OutputDebugStringA(debugBuf);
 		return 0;
 	}
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
 	// Get the address of "LoadLibraryA" of current process
 	DWORD pLoadLibraryA = (DWORD)GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryA");
 	if (pLoadLibraryA == NULL) {
-		sprintf_s(debugBuf, 100, "[Error] Cannot find LoadLibraryA address in kernel32.dll");
+		sprintf_s(debugBuf, 256, "[Error] Cannot find LoadLibraryA address in kernel32.dll");
 		OutputDebugStringA(debugBuf);
 		return 0;
 	}
@@ -34,21 +34,21 @@ int main(int argc, char **argv)
 	DWORD processSecurityAttributes = PROCESS_VM_OPERATION | PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | PROCESS_VM_WRITE;
 	HANDLE rProcessHandle = OpenProcess(processSecurityAttributes, FALSE, (DWORD)pid); // Process security attributes are required for the createRemoteThread hProcess parameter
 	if (rProcessHandle == NULL) {
-		sprintf_s(debugBuf, 100, "[Error] Cannot get handle to remote process with PID: %d", pid);
+		sprintf_s(debugBuf, 256, "[Error] Cannot get handle to remote process with PID: %d", pid);
 		OutputDebugStringA(debugBuf);
 		return 0;
 	}
 
 	LPVOID pDllName = VirtualAllocEx(rProcessHandle, NULL, strlen(injectedDllName), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 	if (pDllName == NULL) {
-		sprintf_s(debugBuf, 100, "[Error] Cannot allocate memory in remote process with PID: %d", pid);
+		sprintf_s(debugBuf, 256, "[Error] Cannot allocate memory in remote process with PID: %d", pid);
 		OutputDebugStringA(debugBuf);
 		return 0;
 	}
 
 	// Write DLL name to allocated memory in the virtual space of the remote process
 	if (!WriteProcessMemory(rProcessHandle, pDllName, injectedDllName, strlen(injectedDllName), NULL)) {
-		sprintf_s(debugBuf, 100, "[Error] Cannot write memory in remote process with PID: %d", pid);
+		sprintf_s(debugBuf, 256, "[Error] Cannot write memory in remote process with PID: %d", pid);
 		OutputDebugStringA(debugBuf);
 		return 0;
 	}
@@ -57,7 +57,7 @@ int main(int argc, char **argv)
 	HANDLE hThread = NULL;
 	hThread = CreateRemoteThread(rProcessHandle, NULL, 0, (LPTHREAD_START_ROUTINE)pLoadLibraryA, pDllName, NULL, NULL);
 	if (hThread == NULL) {
-		sprintf_s(debugBuf, 100, "[Error] Cannot create thread in remote process with PID: %d ,With Error Code: %d", pid, GetLastError());
+		sprintf_s(debugBuf, 256, "[Error] Cannot create thread in remote process with PID: %d ,With Error Code: %d", pid, GetLastError());
 		OutputDebugStringA(debugBuf);
 		return 0;
 	}
