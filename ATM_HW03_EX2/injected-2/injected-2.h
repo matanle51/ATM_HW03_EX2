6f8b1190cf97858715293bff4fbda8a1fc55322b
Tom Mahler <tommahler@gmail.com>
2017-12-22 03:31:05 -0800

    IAT Patching work but crash

--- ATM_HW03_EX2/injected-2/injected-2.h
+++ ATM_HW03_EX2/injected-2/injected-2.h
@@ -5,6 +5,8 @@
 #include "tchar.h"
 #include <iostream>
 
+using namespace std;
+
 void** getFunctionAddress(char const* wantedFunctionName, char const* wantedDllName);
 int WINAPI MyMaliciousFunction(_In_opt_ HWND hWnd, _In_opt_ LPCSTR lpText, _In_opt_ LPCSTR lpCaption, _In_ UINT uType);
 void PatchIAT(char const* const wantedFunctionName, char const* const wantedDllName);
