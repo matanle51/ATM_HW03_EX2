06d7303b8fa020b9f3c0bef1e8917f15f23669ca
David Lazar <davidlazar81@gmail.com>
2017-12-22 12:21:28 +0200

    added IAT patching

---
+++ ATM_HW03_EX2/injected-2/injected-2.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "stdafx.h"
+
+#include <windows.h>
+#include "tchar.h"
+#include <iostream>
+
+void** getFunctionAddress(char const* wantedFunctionName, char const* wantedDllName);
+int WINAPI MyMaliciousFunction(_In_opt_ HWND hWnd, _In_opt_ LPCSTR lpText, _In_opt_ LPCSTR lpCaption, _In_ UINT uType);
+void PatchIAT();
